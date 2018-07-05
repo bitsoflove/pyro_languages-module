aafb3c2abb8ffab6095f4198729c12d53331d0af
Michiel <>
2018-07-05 09:34:38 +0200

    add option handler

--- src/Language/LanguageModel.php
+++ src/Language/LanguageModel.php
@@ -2,10 +2,20 @@
 
 use Bitsoflove\LanguagesModule\Language\Contract\LanguageInterface;
 use Anomaly\Streams\Platform\Model\Languages\LanguagesLanguagesEntryModel;
+use Bitsoflove\LanguagesModule\Language\Handler\Options;
 
 class LanguageModel extends LanguagesLanguagesEntryModel implements LanguageInterface
 {
 
     protected $with = ['translations'];
 
+
+    public function getMultipleFieldTypeOptionsHandler(){
+        return Options::class;
+    }
+
+    public function getRelationshipFieldTypeOptionsHandler(){
+
+        return Options::class;
+    }
 }
